33bca89626dd3d5e054b4076c866c4641f2deabb
alaminAppnap <alamin@appnap.io>
2025-04-16 18:03:00 +0600

    initial commit

---
+++ variable_and_data_type.go
@@ -0,0 +1,24 @@
+package main
+import "fmt"
+
+func main(){
+	/** 
+	 We use data type only
+	 int
+	 float32
+	 bool
+	 string
+	*/
+
+	//Declare and initialize variable
+
+	//int type data different way
+	var x int=10
+	a :=20
+
+	//float32 type data different way
+	var y float32=10.20;
+	 y = 9.99
+
+	fmt.Println(x,a,y)
+}
