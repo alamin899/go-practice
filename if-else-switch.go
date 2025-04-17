3b39b12e655436914e5066d0e9838e5961e58b41
alaminAppnap <alamin@appnap.io>
2025-04-17 15:51:01 +0600

    if else condition and switch case

--- if-else-switch.go
+++ if-else-switch.go
@@ -1,6 +1,58 @@
 package main
+
 import "fmt"
 
-func main(){
-	
+func main() {
+	var age int = 20
+	if age > 18 {
+		fmt.Println("greater than sign implemented")
+	} else if age < 18 {
+		fmt.Println("less than condition implemented")
+	}else if age <= 18 {
+		fmt.Println("lessthan or equal sign implemented")
+	}else if age == 19 {
+		fmt.Println("equal sign impelemted")
+	} else {
+		fmt.Println("no sign")
+	}
+
+	var sex string="male"
+	if age == 20 && sex == "male" {
+		fmt.Println("and sign implemented")
+	} else if age == 20 || sex == "male" {
+		fmt.Println("or sign implemented")
+	} else {
+		fmt.Println("no sign implemented")
+	}
+
+	var swithData int = 50
+
+    // Switch case implementation
+    switch swithData {
+    case 10:
+        fmt.Println("Value is 10")
+    case 20:
+        fmt.Println("Value is 20")
+    case 50:
+        fmt.Println("Value is 50")
+    default:
+        fmt.Println("Value is unknown")
+    }	
+	// Switch case with fallthrough (it will not break after case match it will go next case)
+	switch swithData {
+	case 10:
+		fmt.Println("Value is 10")
+		fallthrough
+	case 20:
+		fmt.Println("Value is 20")
+		fallthrough
+	case 50:
+		fmt.Println("Value is 50")
+		fallthrough
+	case 30:
+		fmt.Println("Value is 50")
+		fallthrough
+	default:
+		fmt.Println("Value is unknown")	
+	}		
 }
--- variable_and_data_type.go
+++ variable_and_data_type.go
@@ -1,24 +1,86 @@
 package main
+
 import "fmt"
 
-func main(){
-	/** 
-	 We use data type only
-	 int
-	 float32
-	 bool
-	 string
+func main() {
+	/**
+	We use data type only
+	int
+	float32
+	bool
+	string
 	*/
 
 	//Declare and initialize variable
 
 	//int type data different way
-	var x int=10
-	a :=20
+	var x int = 10
+	a := 20
 
 	//float32 type data different way
-	var y float32=10.20;
-	 y = 9.99
+	var y float32 = 10.20
+	y = 9.99
 
-	fmt.Println(x,a,y)
+	fmt.Println(x, a, y)
 }
+
+
+/**
+✅ Basic Types
+	var a int = 10          // Integer type (default size based on system)
+	var b float64 = 3.14    // 64-bit floating point number
+	var c bool = true       // Boolean value (true or false)
+	var d string = "Hello"  // A string (sequence of characters)
+	var e byte = 'A'        // Byte is alias for uint8 (represents ASCII characters)
+	var f rune = '😊'       // Rune is alias for int32 (for Unicode characters)
+
+✅ Composite Types
+	// Array: Fixed-size list of integers
+	var arr [3]int = [3]int{1, 2, 3}
+
+
+	// Slice: Dynamic-size list of integers
+	var slice []int = []int{4, 5, 6}
+
+
+	// Struct: Custom data type with named fields
+	type Person struct {
+		Name string
+		Age  int
+	}
+	var p = Person{Name: "John", Age: 30}  // Creating a struct instance
+
+
+	// Map: Key-value pair collection
+	var m map[string]int = map[string]int{"a": 1, "b": 2}
+
+
+	// Pointer: Holds memory address of a variable
+	var x int = 5
+	var ptr *int = &x   // ptr now points to x
+
+
+	// Function: First-class citizen in Go
+	func greet(name string) string {
+		return "Hello " + name
+	}
+
+
+	// Channel: Used for communication between goroutines
+	ch := make(chan int)
+	go func() {
+		ch <- 42        // Sending data to channel
+	}()
+
+
+	// Interface: Defines a set of methods
+	type Speaker interface {
+		Speak() string
+	}
+
+
+✅ Custom Types
+type MyInt int        // Define a new type based on int
+var num MyInt = 25    // Create a variable of type MyInt
+
+*/
